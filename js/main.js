deb721e1391a929252d8f957f42eb426910732e2
Anurag Majumdar <an.mostwanted@gmail.com>
2018-07-22 01:13:38 +0530

    feat: Implement network requests using fetch and improve alt text
    Replaced xhr request with fetch API, handled response and errors
    using then and catch blocks. Configured the url and port of the
    DATABASE_URL to match that of the development server's port.
    Enhanced the alt text of the restaurant's image for both
    restaurant list and restaurant detail pages. Removed unnecessary
    commented code.

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
   fetchNeighborhoods();
   fetchCuisines();
   // Register service worker
-  registerServiceWorker();
+ registerServiceWorker();
 });
 
 /**
@@ -104,18 +104,6 @@ initMap = () => {
 
   updateRestaurants();
 }
-/* window.initMap = () => {
-  let loc = {
-    lat: 40.722216,
-    lng: -73.987501
-  };
-  self.map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: loc,
-    scrollwheel: false
-  });
-  updateRestaurants();
-} */
 
 /**
  * Update page and map for current restaurants.
@@ -137,7 +125,7 @@ updateRestaurants = () => {
       resetRestaurants(restaurants);
       fillRestaurantsHTML();
     }
-  })
+  });
 }
 
 /**
@@ -178,7 +166,7 @@ createRestaurantHTML = (restaurant) => {
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   // Add alt-text for restaurant images according to restaurant names.
-  image.alt = restaurant.name + ' restaurant image';
+  image.alt = `Name of the restaurant: ${restaurant.name}`;
   li.append(image);
 
   const name = document.createElement('h3');
@@ -219,14 +207,3 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 
 }
-/* addMarkersToMap = (restaurants = self.restaurants) => {
-  restaurants.forEach(restaurant => {
-    // Add marker to the map
-    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
-    google.maps.event.addListener(marker, 'click', () => {
-      window.location.href = marker.url
-    });
-    self.markers.push(marker);
-  });
-} */
-
